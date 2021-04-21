8bbb381285c2fae57a05e488a966f4eb265c1378
smulik shotlandgit config --global user.name smulik <shmolik1990@gmail.com>
2021-04-21 22:18:25 +0300

    shmulik1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { InnerRouterComponent } from './inner-router/inner-router.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: InnerRouterComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

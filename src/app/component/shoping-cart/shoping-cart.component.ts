d9c760662d275e2fbbd70308d296e527afca0305
myusername <myemail>
2021-04-26 17:33:57 +0300

    add BehaviorSubject

--- src/app/component/shoping-cart/shoping-cart.component.ts
+++ src/app/component/shoping-cart/shoping-cart.component.ts
@@ -1,40 +1,53 @@
 import { Component, OnInit } from '@angular/core';
 import { Subscription, Observable } from 'rxjs';
-import { ShopingCartService ,ShopingCart} from './../../services/shoping-cart.service';
+import {
+  ShopingCartService,
+  ShopingCart,
+} from './../../services/shoping-cart.service';
 
 @Component({
   selector: 'app-shoping-cart',
   templateUrl: './shoping-cart.component.html',
-  styleUrls: ['./shoping-cart.component.css']
+  styleUrls: ['./shoping-cart.component.css'],
 })
 export class ShopingCartComponent implements OnInit {
-  listShopingCart:ShopingCart[]
-  price:number;
+  listShopingCart: ShopingCart[];
+  price: number;
   subscription: Subscription;
-  constructor(private ShopingCartService:ShopingCartService) { }
+  constructor(private ShopingCartService: ShopingCartService) {}
 
   ngOnInit(): void {
-    this.subscription = this.ShopingCartService.getlistShopingCart()
-    .subscribe(res => {
-      this.price=0
+    this.subscription = this.ShopingCartService.getlistShopingCart().subscribe(
+      (res) => {
+        this.price = 0;
 
-      this.listShopingCart=res;
-      console.log("  this.listShopingCart",  this.listShopingCart);
-      this.listShopingCart.map((itme:ShopingCart)=>{
-        this.price +=  itme.price*itme.amount;
-      })
-      
-    });
-  
-
+        this.listShopingCart = res;
+        console.log('  this.listShopingCart', this.listShopingCart);
+        this.listShopingCart.map((itme: ShopingCart) => {
+          this.price += itme.price * itme.amount;
+        });
+      }
+    );
   }
 
-  deleteItme(itme){
+  updeteamount(id, key) {
+    const itme = this.listShopingCart.filter((e) => e.id == id)[0];
+    switch (key) {
+      case 'ADD':
+        itme.amount++;
+        break;
+      case 'MINS':
+        itme.amount = itme.amount < 2 ? itme.amount : itme.amount - 1;
+        break;
+    }
+    this.ShopingCartService.addToShopingCart(itme);
+  }
+
+  deleteItme(itme) {
     this.ShopingCartService.rmoveItmeCart(itme);
   }
 
   ngOnDestroy() {
     this.subscription.unsubscribe();
-}
-
+  }
 }
